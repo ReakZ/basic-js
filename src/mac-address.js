ff1f87a51431064abf3434df4f0ed12e6b443b82
ReakZ <spec.kov@list.ru>
2022-05-01 21:30:48 +0300

    feat: implement mac-address task

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,10 +14,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+
+  let L=n.split("-").map(x=>x.split("")).flat()
+  
+  for (let index = 0; index < L.length; index++) {
+    const element = L[index];
+    if(!checkSymbol(element)){
+      return false
+    }
+  }
+
+  function checkSymbol(s) {
+    const listSymbols = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+    return listSymbols.includes(s);
+  }
+
+  return true
 }
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
+
+let a='00-1B-63-84-45-E6'
+
+b='Z1-1B-63-84-45-E6'
+c='not a MAC-48 address'
+
+f='FF-FF-FF-FF-FF-FF'
+g='G0-00-00-00-00-00'
+
+console.log(isMAC48Address(b))
