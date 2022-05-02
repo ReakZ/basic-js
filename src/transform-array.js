9323fc907e587a17614cf603f97c9a8d4c2b0e90
ReakZ <spec.kov@list.ru>
2022-05-02 22:02:13 +0300

    feat: solve transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,59 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const DISCARD_ITEM = "discard";
+  let newArray = arr.slice();
+  arr.forEach((x, i) => {
+    switch (x) {
+      case "--discard-next":
+        if (i < newArray.length - 1) {
+          newArray[i + 1] = DISCARD_ITEM;
+        }
+        newArray[i] = DISCARD_ITEM;
+        break;
+
+      case "--discard-prev":
+        if (i > 0) {
+          newArray[i - 1] = DISCARD_ITEM;
+        }
+        newArray[i] = DISCARD_ITEM;
+        break;
+      case "--double-next":
+        if (i < newArray.length - 1) {
+          newArray[i] = newArray[i + 1];
+        } else {
+          newArray[i] = DISCARD_ITEM;
+        }
+
+        break;
+      case "--double-prev":
+        if (i > 0) {
+          newArray[i] = newArray[i - 1];
+        } else {
+          newArray[i] = DISCARD_ITEM;
+        }
+        break;
+      default:
+        break;
+    }
+  });
+
+  return newArray.filter((el) => el !== DISCARD_ITEM);
 }
 
 module.exports = {
